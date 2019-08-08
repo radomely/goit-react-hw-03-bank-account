0528bbe7259a29c643911d8f80c0bf462e5a28e9
Andrii <radomely@gmail.com>
2019-08-08 15:14:23 +0300

    Updates

---
+++ precache-manifest.442ec920385d7513d7de7259fa1794e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441cac5f0dc8f4a3422e163168c7dfca",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "583ef6caa8c02e23f077",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "7e3cf65e3779a2dbb53e",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.ee4de07f.chunk.css"
+  },
+  {
+    "revision": "583ef6caa8c02e23f077",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.5c8852e3.chunk.js"
+  },
+  {
+    "revision": "7e3cf65e3779a2dbb53e",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.a892a56e.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
